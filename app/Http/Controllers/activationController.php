fc3c33db293224db1fd401ea94db917d70607e09
Othman <assaigh.2013@gmail.com>
2021-07-14 15:01:01 +0300

    update activation controller fix bug

--- app/Http/Controllers/activationController.php
+++ app/Http/Controllers/activationController.php
@@ -5,22 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use Senteinal;
-use Activation;
-use App\User ;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+use Cartalyst\Sentinel\Laravel\Facades\Activation;
+use App\User;
 
 class activationController extends Controller
 {
-   public function activate($email $activationCode)
+    public function activate(Request $request)
    {
-          $user=User::whereEmail($email)->first();
+          $user=User::whereEmail($request->email)->first();
           $sentinalUser=findById($user->id);
 
-          if(Activation::complete($sentinalUser,$activationCode))
+          if(Activation::complete($sentinalUser,$request->activationCode))
           {
-
           	return redirect('/login');
-          }
+          }else{
 
-   }else
+          }
+    }
 }
