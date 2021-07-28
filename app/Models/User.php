7a68e2046c228da692347452b2614d951533f89b
Othman <assaigh.2013@gmail.com>
2021-07-28 17:21:48 +0300

    update user model with new fillables

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,51 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 
 class User extends Model
 {
+    use HasApiTokens, HasFactory;
+
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The database primary key value.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'name',
-        'email'
+        'full_name',
+        'email',
+        'phone',
+        'type',
+        'logo',
     ];
 
     public function levels()
