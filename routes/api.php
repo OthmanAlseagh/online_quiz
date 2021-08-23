e8ab7801cfc4f8347a9378f86ae8727c92b19853
Othman <assaigh.2013@gmail.com>
2021-08-23 13:56:43 +0300

    add login route to api

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+
+use App\Http\Controllers\API\LoginController;
+
+//Route::middleware('api')->group(function () {
+    Route::post('login', [LoginController::class, 'login']);
+//});
+
+
 
 
 
