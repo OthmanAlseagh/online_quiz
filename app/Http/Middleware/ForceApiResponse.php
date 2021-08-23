97faae25c54fcce64b39baa49811d9624ddcae5f
Othman <assaigh.2013@gmail.com>
2021-08-23 13:50:36 +0300

    change admin middleware to ForceApiResponse middleware

--- app/Http/Middleware/ForceApiResponse.php
+++ app/Http/Middleware/ForceApiResponse.php
@@ -3,9 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Sentinel;
 
-class AdminMiddleware
+class ForceApiResponse
 {
     /**
      * Handle an incoming request.
@@ -16,9 +15,7 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-       if (Sentinel::Check()&&Sentinel::getUser()->roles()->first()->slug=='admin')
+        $request->headers->set('Accept','application/json');
         return $next($request);
-        else
-            return redirect ('/home');
     }
 }
